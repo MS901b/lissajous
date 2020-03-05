691f65d0bea27cd67bf14f537d983855b0635da7
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-03-05 15:19:09 -0300

    teste

--- scripts_lissajous/correcao_4.js
+++ scripts_lissajous/correcao_4.js
@@ -83,8 +83,8 @@ var parte_2 = new (Class.create({
 		mediaElement.currentTime = 50; // Ir para 122 segundos
 		mediaElement.pause();
 		//mediaElement.currentTime = n-1;
-		//ev.stop();
-		//this.video.vaiProPonto(n-1);
+		ev.stop();
+		this.video.vaiProPonto(n-1);
 	},
 	destaca: function(n)
 	{
